485426872eddf4eac0162f2dac1a11dff63b5bb9
Hasibul Islam Nirob <hasibolislamnirob@gmail.com>
2024-10-15 21:45:50 +0600

    Update VaccineCenterSeeder.php

--- database/seeders/VaccineCenterSeeder.php
+++ database/seeders/VaccineCenterSeeder.php
@@ -16,7 +16,14 @@ class VaccineCenterSeeder extends Seeder
         $centers = [
             ['name' => 'Center 1', 'daily_limit' => 50],
             ['name' => 'Center 2', 'daily_limit' => 60],
-            ['name' => 'Center 3', 'daily_limit' => 40]
+            ['name' => 'Center 3', 'daily_limit' => 40],
+            ['name' => 'Center 4', 'daily_limit' => 80],
+            ['name' => 'Center 5', 'daily_limit' => 30],
+            ['name' => 'Center 6', 'daily_limit' => 10],
+            ['name' => 'Center 7', 'daily_limit' => 20],
+            ['name' => 'Center 8', 'daily_limit' => 70],
+            ['name' => 'Center 9', 'daily_limit' => 30],
+            ['name' => 'Center 10', 'daily_limit' => 50],
         ];
 
         foreach ($centers as $center) {
